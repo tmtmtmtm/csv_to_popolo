6ec4ef4edf0f5370fb2738da1b4b3c7af62fc58e
Tony Bowden <tony@tmtm.com>
2015-02-26 16:50:24 +0000

    split tests slightly
    separate change to make it more obvious what the later change is doing.

--- t/test_riigikogu.rb
+++ t/test_riigikogu.rb
@@ -11,31 +11,40 @@ describe "riigikogu" do
     Popolo::CSV.from_file('t/data/riigikogu-members.csv')
   }
 
-  let(:arto)  { subject.data[:persons].find { |i| i[:name] == 'Arto Aas' } }
-  let(:mems)  { subject.data[:memberships].find_all   { |m| m[:person_id] == arto[:id] } }
   let(:orgs)  { subject.data[:organizations] }
 
-  it "should have a record" do
-    arto.class.must_equal Hash
+  describe "arto" do
+
+    let(:arto)  { subject.data[:persons].find { |i| i[:name] == 'Arto Aas' } }
+    let(:mems)  { subject.data[:memberships].find_all { |m| m[:person_id] == arto[:id] } }
+
+    it "should have a record" do
+      arto.class.must_equal Hash
+    end
+
+    it "should have the correct id" do
+      arto[:id].must_equal 'fe748f4d-3f50-4af8-8069-92a460978d2b'
+    end
+
+    it "should have nested faction info" do
+      mems.count.must_equal 2
+      party_mem = mems.find { |m| m[:role] == 'party representative' }
+      party = orgs.find { |o| party_mem[:organization_id] == o[:id] }
+      party[:name].must_equal 'Eesti Reformierakonna fraktsioon'
+      party[:classification].must_equal 'party'
+    end
+
   end
 
-  it "should have the correct id" do
-    arto[:id].must_equal 'fe748f4d-3f50-4af8-8069-92a460978d2b'
-  end
+  describe "validation" do
 
-  it "should have nested faction info" do
-    mems.count.must_equal 2
-    party_mem = mems.find { |m| m[:role] == 'party representative' }
-    party = orgs.find { |o| party_mem[:organization_id] == o[:id] }
-    party[:name].must_equal 'Eesti Reformierakonna fraktsioon'
-    party[:classification].must_equal 'party'
-  end
-
-  it "should validate" do
-    json = JSON.parse(subject.data.to_json)
-    %w(person organization membership).each do |type|
-      JSON::Validator.fully_validate("http://www.popoloproject.com/schemas/#{type}.json", json[type + 's'], :list => true).must_be :empty?
+    it "should validate" do
+      json = JSON.parse(subject.data.to_json)
+      %w(person organization membership).each do |type|
+        JSON::Validator.fully_validate("http://www.popoloproject.com/schemas/#{type}.json", json[type + 's'], :list => true).must_be :empty?
+      end
     end
   end
+
 end
 
