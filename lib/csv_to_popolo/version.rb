06e5f9ac0df8cb9fcd2511e0d076960eb0c7428f
Tony Bowden <tony@tmtm.com>
2015-03-16 10:23:34 +0000

    downcase the headers
    Convert the headers in the incoming CSV to lower case (and ensure we
    have a test case with mac-style \r line endings)

--- lib/csv_to_popolo/version.rb
+++ lib/csv_to_popolo/version.rb
@@ -1,3 +1,3 @@
 module Popolo_CSV
-  VERSION = "0.7.1"
+  VERSION = "0.7.2"
 end
---
+++ t/test_mixed_case.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+
+require 'csv_to_popolo'
+require 'minitest/autorun'
+require 'json'
+require 'json-schema'
+
+describe "mac" do
+
+  subject     { Popolo::CSV.new('t/data/mac.csv') }
+
+  describe "counts" do
+
+    it "should have two people" do
+      subject.data[:persons].count.must_equal 2
+    end
+
+    it "should have correct gender" do
+      subject.data[:persons].last[:gender].must_equal 'Female'
+    end
+
+  end
+
+  describe "validation" do
+
+    it "should have no warnings" do
+      subject.data[:warnings].must_be_nil
+    end
+
+  end
+
+end
+
