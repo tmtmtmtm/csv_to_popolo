01a9250ccd44cacc3fb7ecf46e3d1aa3564bf14c
Tony Bowden <tony@tmtm.com>
2015-05-03 12:52:43 +0100

    Rename the 'broken' CSV files
    Prefix any CSV files that should NOT load cleanly with “broken-“ so we
    can eliminate them from automatic checking.

--- t/test_dupe_headers.rb
+++ t/test_dupe_headers.rb
@@ -5,7 +5,7 @@ require 'minitest/autorun'
 
 describe "duplicate columns" do
 
-  subject     { Popolo::CSV.new('t/data/duplicate_headers.csv') }
+  subject     { Popolo::CSV.new('t/data/broken_duplicate_headers.csv') }
 
   it "should warn about the duplicate name column" do
     subject.data[:warnings][:dupes].must_include :name
