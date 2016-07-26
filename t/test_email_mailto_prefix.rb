b8faee921ac61c3ff867858ec0f8df37637e8b14
Oliver Denman <oliver@therewillbeanimals.com>
2016-07-26 11:29:20 +0100

    add tests to check that email addresses are not prefixed with mailto

---
+++ t/test_email_mailto_prefix.rb
@@ -0,0 +1,23 @@
+
+require 'csv_to_popolo'
+require 'minitest/autorun'
+
+describe 'email: value' do
+  subject { Popolo::CSV.new('t/data/dominican_republic.csv') }
+  let(:persons) { subject.data[:persons] }
+  let(:person1) { persons[2] }
+
+  it 'should not be prefixed with mailto:' do
+    person1[:email].wont_include "mailto:"
+  end
+end
+
+describe ':contact_details email value' do
+  subject { Popolo::CSV.new('t/data/dominican_republic.csv') }
+  let(:persons) { subject.data[:persons] }
+  let(:person1) { persons[2] }
+
+  it 'should not be prefixed with mailto:' do
+    person1[:contact_details][0][:value].wont_include "mailto:"
+  end
+end
